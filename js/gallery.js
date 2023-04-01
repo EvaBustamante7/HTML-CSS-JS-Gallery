2749135485decbbc67cda2fdc7288921a65d1fed
Eva Bustamante <evabrodriguez7@gmail.com>
2023-04-01 22:43:23 +0200

    starting to create Gallery

---
+++ js/gallery.js
@@ -0,0 +1,7 @@
+let images = [...document.querySelectorAll('.img')];
+let slider = document.querySelector('.slider');
+let sliderWidth;
+let imageWidth;
+let current = 0;
+let target = 0;
+let ease = .05;
