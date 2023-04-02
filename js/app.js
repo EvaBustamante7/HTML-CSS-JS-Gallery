026812d84ae11ae574da53aa7d87497af404f373
Eva Bustamante <evabrodriguez7@gmail.com>
2023-04-02 20:22:40 +0200

    finished gallery js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ let sliderWidth;
 let imageWidth;
 let current = 0;
 let target = 0;
-let ease = .05;
+let ease = .04;
+
+window.addEventListener('resize', init);
 
 images.forEach((img, idx) => {
     img.style.backgroundImage = `url(./img_gallery/${idx+1}.jpg)`
@@ -24,4 +26,23 @@ function init(){
     document.body.style.height = `${sliderWidth - (window.innerWidth - window.innerHeight)}px`
 }
 
+function animate(){
+    current = parseFloat(lerp(current, target, ease)).toFixed(2);
+    target = window.scrollY;
+    setTransform(slider, `translateX(-${current}px)`);
+    animateImages();
+    requestAnimationFrame(animate);
+}
+
+function animateImages(){
+    let ratio = current / imageWidth;
+    let intersectionRatioValue;
+
+    images.forEach((image, idx) => {
+        intersectionRatioValue = ratio - (idx * 0.7);
+        setTransform(image, `translateX(${intersectionRatioValue * 70}px)`)
+    })
+}
+
 init();
+animate();
