19ff9efba82effe63b697e9e519d5fe7e26ef936
Eva Bustamante <evabrodriguez7@gmail.com>
2023-04-03 15:13:01 +0200

    img

--- js/main.js
+++ js/main.js
@@ -6,9 +6,3 @@ ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.gallery', {delay: 70});
 ScrollReveal().reveal('.banner1', {delay: 70});
 ScrollReveal().reveal('.banner2', {delay: 70});
-
-document.addEventListener("mousemove", function(e) {
-    var customCursor = document.getElementById("custom-cursor");
-    customCursor.style.left = e.clientX + "px";
-    customCursor.style.top = e.clientY + "px";
-  });
